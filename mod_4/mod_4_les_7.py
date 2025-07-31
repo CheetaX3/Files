4084c78a14efb6eb8335dbc69657c3f940296248
CheetahtraderX <grandvitara2011@gmail.com>
2025-07-31 21:28:08 +0300

    На проверку, хотя что тут проверять)

---
+++ mod_4/mod_4_les_7.py
@@ -0,0 +1,37 @@
+# В этом классе мы будем использовать список для хранения элементов очереди
+class Queue:
+    def __init__(self):
+        self.items = []
+
+# Метод is_empty, который будет проверять, есть ли элементы в очереди
+    def is_empty(self):
+        return len(self.items) == 0
+
+# Метод enqueue, который будет добавлять элемент в очередь. 
+# Мы будем добавлять элемент в конец списка
+    def enqueue(self, item):
+        self.items.append(item)
+
+# Метод dequeue, который будет удалять и возвращать элемент из вершины стека. 
+# При этом проверим, не пуст ли стек перед попыткой извлечения элемента
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            raise IndexError("В очереди нет элементов.")
+
+# метод size, который будет возвращать количество элементов в стеке
+    def size(self):
+        return len(self.items)
+
+# Пример использования:
+queue = Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+
+print("Размер очереди:", queue.size())  # Размер очереди: 3
+
+while not queue.is_empty():
+    item = queue.dequeue()
+    print("Извлечен элемент:", item)
