177e3fcee7c72572fc045b4fe92a5c986c23f992
CheetahtraderX <grandvitara2011@gmail.com>
2025-08-11 01:51:38 +0300

    Реализация графа через классы графа и узла

---
+++ mod_4/mod_4_les_13.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+
+        self.outbound = []
+        self.inbound = []
+
+    def point_to(self, other):
+        self.outbound.append(other)
+        other.inbound.append(self)
+
+    def __repr__(self):
+        return f'Node({self.value})'
+
+class Graph:
+    def __init__(self, root):
+        self._root = root
+
+    def dfs(self, vertex=None, visited=None):
+        if visited is None:
+            visited = []
+        if vertex is None:
+            vertex = self._root
+
+        if vertex not in visited:
+            visited.append(vertex)
+            for neighbor in vertex.outbound:
+                self.dfs(neighbor, visited)
+        return visited
+
+# Проверка
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+a.point_to(b)
+b.point_to(c)
+c.point_to(d)
+d.point_to(a)
+b.point_to(d)
+
+g = Graph(a)
+
+print(g.dfs())
