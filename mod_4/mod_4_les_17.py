4e915343cf686a9051ea0626b06fd412d4fd96f5
CheetahtraderX <grandvitara2011@gmail.com>
2025-08-15 21:51:57 +0300

    сортировка пузырьком

---
+++ mod_4/mod_4_les_17.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    n = len(arr)
+    while n > 0:
+        for i in range(n - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        n -= 1
+
+# Пример использования:
+my_list = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(my_list)
+print("Отсортированный список:", my_list)
