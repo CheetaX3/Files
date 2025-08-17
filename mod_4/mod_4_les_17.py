a43e48b0a918bff8cb78390ed7c6fbce595ddfe0
CheetahtraderX <grandvitara2011@gmail.com>
2025-08-17 14:49:42 +0300

    корректировка условия: замена n>0 на n>1

--- mod_4/mod_4_les_17.py
+++ mod_4/mod_4_les_17.py
@@ -1,6 +1,6 @@
 def bubble_sort(arr):
     n = len(arr)
-    while n > 0:
+    while n > 1:
         for i in range(n - 1):
             if arr[i] > arr[i + 1]:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
