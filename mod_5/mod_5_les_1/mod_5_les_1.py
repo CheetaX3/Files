a7ad79acbb261fb3250b6051d6e1fb5e57187d52
CheetahtraderX <grandvitara2011@gmail.com>
2025-09-03 23:13:46 +0400

    убрал фиксированный путь к файлу, оптимизировал функцию get_words_dict

--- mod_5/mod_5_les_1/mod_5_les_1.py
+++ mod_5/mod_5_les_1/mod_5_les_1.py
@@ -8,15 +8,12 @@ def get_words(filename: str) -> list:
 def get_words_dict(words):
     words_dict = {}
     for word in words:
-        if word in words_dict:
-            words_dict[word] += 1
-        else:
-            words_dict[word] = 1
+        words_dict[word] = words_dict.get(word, 0) + 1
     return words_dict
 
 def main():
-    filename = input('Введите имя файла: ')
-    words = get_words(fr'mod_5\mod_5_les_1\{filename}.txt')
+    filename = input('Введите имя файла: ')
+    words = get_words(f'{filename}.txt')
     words_dict = get_words_dict(words)
     print(f'Количество слов в файле: {len(words)}')
     print(f'Количество уникальных слов в файле: {len(words_dict)}')
