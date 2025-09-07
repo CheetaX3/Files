1f8b2cbb8137a5bc166e9e8100eb5b5b5f496fd7
CheetahtraderX <grandvitara2011@gmail.com>
2025-09-07 15:19:27 +0400

    Задания по модулю datetime

---
+++ mod_5/mod_5_les_6/mod_5_les_6.py
@@ -0,0 +1,26 @@
+import datetime as dt
+import locale
+
+
+locale.setlocale(locale.LC_TIME, "Russian_Russia.1251")
+
+now_date = dt.datetime.now()
+print("День по счету:", now_date.day)
+
+today_date = dt.datetime.today()
+print("День недели:", today_date.strftime("%A"))
+
+if now_date.year % 4 == 0 and (now_date.year % 100 != 0 or now_date.year % 400 == 0):
+    print('Текущий год високосный')
+else:
+    print('Текущий год не високосный')
+
+new_date = input('Введите дату в формате ГГГГ-ММ-ДД: ')
+new_date = dt.datetime.strptime(new_date, "%Y-%m-%d")
+difference = new_date - now_date
+
+days = difference.days
+hours, remainder = divmod(difference.seconds, 3600)
+minutes, _ = divmod(remainder, 60)
+
+print(f"Разница: {days} дней, {hours} часов, {minutes} минут")
