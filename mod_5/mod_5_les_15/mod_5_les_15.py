8a282d03da74ecef313c894d536a3d762ed934ff
CheetahtraderX <grandvitara2011@gmail.com>
2025-09-25 20:41:05 +0300

    Практикум 4

---
+++ mod_5/mod_5_les_15/mod_5_les_15.py
@@ -0,0 +1,50 @@
+# Задача 1
+students_dict = {
+ 'Саша': 27,
+ 'Кирилл': 52,
+ 'Маша': 14,
+ 'Петя': 36,
+ 'Оля': 43,
+}
+
+sorted_students = sorted(students_dict.items(), key=lambda x: x[1])
+print(sorted_students)
+
+# Задача 2
+data = [
+    (82, 191),
+    (68, 174),
+    (90, 189),
+    (73, 179),
+    (76, 184)
+]
+
+sorted_data = sorted(data, key=lambda x: x[0]/(x[1]/100 ** 2))
+print(sorted_data)
+
+# Задача 3
+students_list = [
+    {
+        "name": "Саша",
+        "age": 27,
+    },
+    {
+        "name": "Кирилл",
+        "age": 52,
+    },
+    {
+        "name": "Маша",
+        "age": 14,
+    },
+    {
+        "name": "Петя",
+        "age": 36,
+    },
+    {
+        "name": "Оля",
+        "age": 43,
+    },
+]
+
+youngest_student = min(students_list, key=lambda x: x["age"])
+print(youngest_student)
