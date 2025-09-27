35331a3b653e497d88e5d82200b67947fcbe2651
CheetahtraderX <grandvitara2011@gmail.com>
2025-09-27 20:39:13 +0300

    заменил лямбды на обычные функции

--- mod_5/mod_5_les_13/mod_5_les_13.py
+++ mod_5/mod_5_les_13/mod_5_les_13.py
@@ -1,15 +1,33 @@
 from functools import reduce
 
 
+def pow_3(x):
+    return x ** 3
+
+
 numbers1 = [1, 2, 3]
-powed_numbers1 = map(lambda x: pow(x, 2), numbers1)
-print(list(powed_numbers1))
+new_numbers1 = map(pow_3, numbers1)
+print(list(new_numbers1))
+
+
+def multiple_of_5(x):
+    return x % 5 == 0
+
 
 numbers2 = [5, 2, 3, 10]
-filtered_numbers2 = filter(lambda x: x % 5 == 0, numbers2)
-print(list(filtered_numbers2))
+new_numbers2 = filter(multiple_of_5, numbers2)
+print(list(new_numbers2))
+
+
+def is_not_even(x):
+    return x % 2 != 0
+
+
+def multiply(x, y):
+    return x * y
+
 
 numbers3 = [5, 2, 3, 10]
-filtered_numbers3 = filter(lambda x: x % 2 != 0, numbers3)
-powed_numbers3 = reduce(lambda x, y: x * y, filtered_numbers3)
-print(powed_numbers3)
+filtered_numbers3 = filter(is_not_even, numbers3)
+new_numbers3 = reduce(multiply, filtered_numbers3)
+print(new_numbers3)
